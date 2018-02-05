81531e51591c4feb4da82f99760cdc8670f5f322
Dawid Lipski <dawidlipski89@gmail.com>
2018-02-05 14:49:02 +0100

    page layout + basic routing

--- NewsFeed/src/app/app.module.ts
+++ NewsFeed/src/app/app.module.ts
@@ -1,18 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, ModuleWithProviders } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { RouterModule } from '@angular/router';
 
+import {
+  SharedModule,
+  LayoutHeaderComponent,
+  LayoutFooterComponent
+} from './shared';
 
 import { AppComponent } from './app.component';
 
+const rootRouting: ModuleWithProviders = RouterModule.forRoot([], { useHash: true });
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutHeaderComponent,
+    LayoutFooterComponent
   ],
   imports: [
     BrowserModule,
-    NgbModule.forRoot()
+    NgbModule.forRoot(),
+    SharedModule,
+    rootRouting
   ],
   providers: [],
   bootstrap: [AppComponent]
